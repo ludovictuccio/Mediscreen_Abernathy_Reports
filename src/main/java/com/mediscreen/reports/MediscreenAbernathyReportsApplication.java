7884e0fb8f7d62df1ffa1ca7da1a35176a9dfa9e
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2021-01-25 10:25:46 +0100

    First configuration

---
+++ src/main/java/com/mediscreen/reports/MediscreenAbernathyReportsApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenAbernathyReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenAbernathyReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediscreen/reports/MediscreenAbernathyReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenAbernathyReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
